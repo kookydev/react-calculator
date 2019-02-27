dd1434baa8445ed99caac89c226c86e3db609000
Code Nation 19 <codenation@CN-19.lan>
2019-02-27 09:31:14 +0000

    layout improvements

--- src/components/NumberButtons.jsx
+++ src/components/NumberButtons.jsx
@@ -3,15 +3,27 @@ import "./Buttons.css";
 import Display from "./Display";
 
 const NumberButtons = props => {
-  return (
-    <div
-      onClick={Display.digitAdder}
-      value={props.value}
-      className="number-buttons"
-    >
-      {props.value}
-    </div>
-  );
+  if (props.value === "0") {
+    return (
+      <div
+        onClick={Display.digitAdder}
+        value={props.value}
+        className="number-buttons zero-button"
+      >
+        {props.value}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        onClick={Display.digitAdder}
+        value={props.value}
+        className="number-buttons"
+      >
+        {props.value}
+      </div>
+    );
+  }
 };
 
 export default NumberButtons;
