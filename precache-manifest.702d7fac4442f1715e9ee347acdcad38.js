30d37475e6df5cff53e2dd08e03c76eb5c654170
Code Nation 19 <codenation@CN-19.lan>
2019-02-27 17:07:01 +0000

    Updates

---
+++ precache-manifest.702d7fac4442f1715e9ee347acdcad38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.6473c924.js"
+  },
+  {
+    "revision": "0fcf62470257dae84028",
+    "url": "/react-calculator/static/js/main.0fcf6247.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/react-calculator/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "0fcf62470257dae84028",
+    "url": "/react-calculator/static/css/main.7f13be62.chunk.css"
+  },
+  {
+    "revision": "e17258230752d396151d763d6c697bfc",
+    "url": "/react-calculator/index.html"
+  }
+];
